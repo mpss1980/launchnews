22d9ecebf235b91292ec187be44d04a70b9ee9ab
mpss1980 <marcosp.sousa@gmail.com>
2022-07-01 10:30:43 -0300

    finished search

--- app/src/main/java/br/com/coupledev/launchnews/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/br/com/coupledev/launchnews/presentation/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import br.com.coupledev.launchnews.R
 import br.com.coupledev.launchnews.core.State
@@ -20,17 +21,62 @@ class HomeFragment : Fragment() {
         HomeFragmentBinding.inflate(layoutInflater)
     }
 
+    private lateinit var searchView: SearchView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         initBinding()
         initSnackbar()
         initRecyclerView()
         initOptionMenu()
+        initSearchBar()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initQueryHintObserver()
     }
 
     private fun initBinding() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
+    }
+
+    private fun initQueryHintObserver() {
+        viewModel.category.observe(viewLifecycleOwner) {
+            searchView.queryHint = getString(R.string.search_in) + when(it) {
+                SpaceFlightNewsCategory.ARTICLES -> getString(R.string.news)
+                SpaceFlightNewsCategory.BLOGS -> getString(R.string.blogs)
+                SpaceFlightNewsCategory.REPORTS -> getString(R.string.reports)
+                else -> ""
+            }
+        }
+    }
+
+    private fun initSearchBar() {
+        with(binding.homeToolbar) {
+            val searchItem = menu.findItem(R.id.actionSearch)
+            searchView = searchItem.actionView as SearchView
+
+            searchView.isIconified = false
+
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    val searchString = searchView.query.toString()
+                    viewModel.searchPostsTitleContains(searchString)
+                    searchView.clearFocus()
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    newText?.let { value ->
+                        viewModel.searchPostsTitleContains(value)
+                    }
+                    return true
+                }
+
+            })
+        }
     }
 
     private fun initOptionMenu() {
